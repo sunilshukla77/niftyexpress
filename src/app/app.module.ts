ad744dd9d2f1780deef956e5eeb1be92df9577e3
sunil <sunil.jobs77@gmail.com>
2020-10-27 20:46:34 +0530

    basic code

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { RegistrationComponent } from './signup/registration/registration.component';
+import { LoginComponent } from './signin/login/login.component';
+import { HomeComponent } from './home/home/home.component';
+import { ContactusComponent } from './contact/contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
