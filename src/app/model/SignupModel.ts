764dd564e7fbf679b497a30a7c9759e016045211
sunil <sunil.jobs77@gmail.com>
2020-11-03 21:37:20 +0530

    added Singup page databinding

---
+++ src/app/model/SignupModel.ts
@@ -0,0 +1,8 @@
+export class SignupModel {
+    fname: string;
+    lname: string;
+    emailId: string;
+    username: string;
+    password: string;
+    mobileNo: string;
+}
--- src/app/signup/registration.component.ts
+++ src/app/signup/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SignupModel } from '../model/SignupModel';
+import { SignupService } from '../service/signup.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  private _signup: SignupModel = {
+    fname: null,
+    lname: null,
+    username: null,
+    emailId: null,
+    password: null,
+    mobileNo: null
+  };
+
+  public get signup() : SignupModel {
+    return this._signup;
+  }
+
+  public set signup(value : SignupModel) {
+    this._signup = value;
+  }
+
+  constructor(private signupService: SignupService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+
   }
 
 }
