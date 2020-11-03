56642f41779dc20c697b749b4e3c65c2faf102c3
sunil <sunil.jobs77@gmail.com>
2020-11-03 22:07:53 +0530

    added moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
