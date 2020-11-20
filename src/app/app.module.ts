7eb5cd8ad9d01045630723841ae72308327a93ea
sunil <sunil.jobs77@gmail.com>
2020-11-20 21:54:06 +0530

    revert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RegistrationComponent } from './signup/registration.component';
@@ -9,23 +11,19 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { FooterComponent } from './footer/footer.component';
+import { SignupService } from './service/signup.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RegistrationComponent,
-    LoginComponent,
-    HomeComponent,
-    ContactusComponent,
-    FooterComponent
-  ],
+  declarations: [  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SignupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
